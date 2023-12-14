b55e88f7a1d660b9ba8334cefca64ceccf597e66
Kayleigh <kaylvigh@gmail.com>
2023-12-14 13:02:02 -0330

    Sectioned things into files a little neater, added controllers, controllers are not finished though

--- .storage/AirlineRepository.java
+++
@@ -1,9 +0,0 @@
-package com.keyin.Airline.Repository;
-
-import com.keyin.Airline.Airline;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AirlineRepository extends CrudRepository<Airline, Long> {
-}
